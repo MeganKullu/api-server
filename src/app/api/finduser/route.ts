a7cf608406ff6accbdae9d60efde4e73f75a815e
MeganKullu <kullumegan@gmail.com>
2024-06-30 21:41:53 +0300

    added finduser route

---
+++ src/app/api/finduser/route.ts
@@ -0,0 +1,32 @@
+import { NextResponse, NextRequest } from "next/server";
+import { users } from "@/src/db/db";
+
+export async function POST(req: NextRequest, res: NextResponse) {
+  try {
+    const { username } = await req.json();
+
+    const userslist = await users.findMany({
+      where: {
+        username: {
+          contains: username,
+          mode: "insensitive",
+        },
+      },
+    });
+
+    if (userslist.length > 0) {
+      return NextResponse.json(
+        { Message: "User found" },
+        { status: 200 },
+        { users: userslist }
+      );
+    } else {
+      return NextResponse.json({ Message: "User not found" }, { status: 404 });
+    }
+  } catch (error) {
+    return NextResponse.json(
+      { Message: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
