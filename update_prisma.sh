1200ee9007734514fc37bfd900360f0987022d33
MeganKullu <kullumegan@gmail.com>
2024-06-30 09:20:47 +0300

    edited package.json

---
+++ update_prisma.sh
@@ -0,0 +1,6 @@
+# /home/kullumegan/Chat-server/update_prisma.sh
+#!/bin/bash
+
+# Copy the Prisma schema
+cp /home/kullumegan/Chat-server/src/prisma/schema.prisma /home/kullumegan/Chat-server/socket-server/
+
