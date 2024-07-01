2937006ee9e7ba733c2ad05d86bbade3e186b0cc
MeganKullu <kullumegan@gmail.com>
2024-07-01 10:54:15 +0300

    added checkrequest route

---
+++ src/app/api/checkrequest/route.tsx
@@ -0,0 +1,52 @@
+import { NextRequest, NextResponse } from "next/server";
+import { friendRequests, users } from "@/src/db/db";
+
+export async function POST(req: NextRequest) {
+    try {
+        const requestBody = await req.json();
+        const { firebaseId, receiverUsername } = requestBody;
+
+        const sender = await users.findUnique({
+            where: { firebaseId },
+          });
+
+        if (!sender) {
+            return NextResponse.json(
+              { Message: "Sender User does not exist" },
+              { status: 404 }
+            );
+          }
+
+        const receiver = await users.findUnique({
+            where: { username: receiverUsername },
+          });
+
+        if (!receiver) {
+            return NextResponse.json(
+              { Message: "Receiver User does not exist" },
+              { status: 404 }
+            );
+          }
+
+          const existingRequest = await friendRequests.findFirst({
+            where: {
+              senderId: sender.id,
+              receiverId: receiver.id,
+            },
+          });
+
+         if (existingRequest) {
+            return NextResponse.json(
+              { Message: "Friend request already sent", friendRequest: existingRequest.status},
+              { status: 201 }
+            );
+          }
+    
+    } catch (error) {
+        return NextResponse.json(
+            { Message: "Internal Server Error" },
+            { status: 500 }
+          );
+        
+    }
+}
