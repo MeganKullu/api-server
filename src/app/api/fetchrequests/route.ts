4df63aa2f8b5f4f326ab1de2a0c5a0cbdc56f4f4
MeganKullu <kullumegan@gmail.com>
2024-07-01 15:10:48 +0300

    edited fetchrequests eport function

--- src/app/api/fetchrequests/route.ts
+++ src/app/api/fetchrequests/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { friendRequests, users } from "@/src/db/db";
 
-export default async function POST(req: NextRequest){
+export async function POST(req: NextRequest){
     try {
         const requestBody = await req.json();
         const { firebaseId} = requestBody;
