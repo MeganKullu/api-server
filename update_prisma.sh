3d5b59f240e9efd621600857ef7fc77e90f2a446
MeganKullu <kullumegan@gmail.com>
2024-07-08 12:10:25 +0300

    edited script path

--- update_prisma.sh
+++ update_prisma.sh
@@ -2,5 +2,5 @@
 #!/bin/bash
 
 # Copy the Prisma schema
-cp /home/kullumegan/Chat-server/src/prisma/schema.prisma /home/kullumegan/socket-server/
+cp /home/kullumegan/api-server/src/prisma/schema.prisma /home/kullumegan/socket-server/
 
